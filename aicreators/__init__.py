91cee8558e7475a685658da9133fc550316b4528
Christofanis Skordas <skordasc@uchicago.edu>
2026-09-02 22:52:49 -0500

    fixes dev bug

--- aicreators/__init__.py
+++ aicreators/__init__.py
@@ -15,17 +15,17 @@ def _build_parser() -> ArgumentParser:
     serve.add_argument("--host", default="0.0.0.0", help="Bind host")
     serve.add_argument("--port", type=int, default=5000, help="Bind port")
     serve.add_argument("--dev", action="store_true", help="Enable Flask debug mode")
-    serve.set_defaults(func=_serve_devel)
+    serve.set_defaults(func=_serve)
 
     return parser
 
 
-def _serve_devel(args: Namespace) -> None:
-    """Starts the web server in development mode."""
+def _serve(args: Namespace) -> None:
+    """Starts the web server."""
     from aicreators.web import create_web_app
 
     app = create_web_app()
-    app.run(app.run(host=args.host, port=args.port, debug=args.dev))
+    app.run(host=args.host, port=args.port, debug=args.dev)
 
 
 def main():
